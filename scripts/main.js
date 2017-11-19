5fbf250039d4cf462f6c7c3d58e0c30cd1c86465
Joseph Pamintuan <you@example.com>
2017-11-19 12:48:58 -0800

    first

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+var messagesRef = firebase.datebase().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+//  Submit form
+function submitForm(e) {
+
+  e.preventDefault();
+
+// Get values
+
+var name = getInputVal('name');
+var company = getInputVal('company');
+var email = getInputVal('email');
+var phone = getInputVal('phone');
+var phone = getInputVal('message');
+  
+  console.log(name);
+
+// Save message
+saveMessage(name, company, email, phone,message);
+
+}
+
+// function to get form values
+function getInputVal(id)  {
+  return document.getElementById(id).value;
+}
+
+
+// Save message to firebase
+
+function saveMessage(name, company, email, phone,message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company: company,
+    email: email,
+    phone: phone,
+    message: message
+
+  });
+  
+}
